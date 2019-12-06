784d65ba02d989a78376c0445dc84e189c6bbac9
Adrian Ponce <adrian.ponce.89@gmail.com>
2019-12-06 15:37:57 -0300

    Integrated morgan to app

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const morgan = require('morgan');
+
 const express = require('express');
 const app = express();
 
 // importing routes
 const products = require('./routes/products');
 const orders = require('./routes/orders');
+
+// middlewares
+app.use(morgan('dev'));
 
 // routes
 app.use('/products', products);
