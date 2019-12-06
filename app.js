aa53a52bf0b869697f4cb6b687ede84645847811
Adrian Ponce <adrian.ponce.89@gmail.com>
2019-12-06 14:40:49 -0300

    Add  product routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: 'It works',
-  });
-});
+// importing routes
+const products = require('./routes/products');
+
+// routes
+app.use('/products', products);
 
 module.exports = app;
